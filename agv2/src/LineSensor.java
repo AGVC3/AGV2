ac305a009608fda5c978ad3e4ca719de2c4aea8f
GamerWoutNL <37815497+GamerWoutNL@users.noreply.github.com>
2018-12-06 13:36:22 +0100

    Callbacks

--- agv2/src/LineSensor.java
+++ agv2/src/LineSensor.java
@@ -1,10 +1,33 @@
+import TI.BoeBot;
+
+import java.util.ArrayList;
+
 public class LineSensor implements Updatable {
 
-    public LineSensor() {
+    private LineSensorCallback lineDetect;
 
+    public LineSensor(LineSensorCallback lineDetect) {
+        this.lineDetect = lineDetect;
     }
 
     public void update() {
+        ArrayList<Integer> linesDetected = new ArrayList<>();
+        if (BoeBot.analogRead(0) > 1000) {
+            linesDetected.add(1);
+        } else {
+            linesDetected.add(0);
+        }
+        if (BoeBot.analogRead(1) > 1000) {
+            linesDetected.add(1);
+        } else {
+            linesDetected.add(0);
+        }
+        if (BoeBot.analogRead(2) > 1000) {
+            linesDetected.add(1);
+        } else {
+            linesDetected.add(0);
+        }
 
+        this.lineDetect.lineSensorDetect(linesDetected);
     }
 }
