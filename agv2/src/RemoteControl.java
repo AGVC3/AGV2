e69f99a396e89649819f981f2ec037a54e929f80
Kirstabro <kirstabro@gmail.com>
2018-12-12 15:30:25 +0100

    RemoteControl

--- agv2/src/RemoteControl.java
+++ agv2/src/RemoteControl.java
@@ -1,10 +1,35 @@
-public class RemoteControl implements Updatable {
+public class RemoteControl {
 
-    public RemoteControl() {
+    private Driver driver;
 
+    public RemoteControl(Driver driver) {
+        this.driver = driver;
     }
 
-    public void update() {
-
+    public void dataToAction(char character) {
+        switch (character) {
+            case 'w':
+                this.driver.goToSpeed(1550);
+                break;
+            case 's':
+                this.driver.goToSpeed(1450);
+                break;
+            case 'a':
+                this.driver.turn("Left");
+                break;
+            case 'd':
+                this.driver.turn("Right");
+                break;
+            case 'f':
+                this.driver.goToSpeed(1500);
+                break;
+            case 'g':
+                this.driver.emergencyBreak();
+                break;
+            default:
+                break;
+        }
     }
+
+
 }
